0709eef365c28edd201d57e224c45aeb5db71c9e
Mayuraksha Sikdar <mayurakshasikdar@gmail.com>
2019-10-17 22:41:44 +0530

    Added 1. image view functionality, 2. Post model 3. Post submit validation 4. DB store

--- app.js
+++ app.js
@@ -1,10 +1,48 @@
+const path = require('path');
+
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const feedController = require('./routes/feed');
 
+const MONGODB_URI =
+    'mongodb+srv://Mayuraksha:Sunnytonny1@cluster0-jyoeh.mongodb.net/messages?retryWrites=true&w=majority';
+
 const app = express();
+
+
+
+app.use(bodyParser.json());
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
 app.use('/feed', feedController);
 
-app.listen(8000);
-console.log('CONNECTED');
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    res.status(status).json({
+        message: message
+    });
+})
+
+mongoose.connect(MONGODB_URI, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+    })
+    .then(result => {
+        app.listen(8080);
+        console.log('CONNECTED');
+    })
+    .catch(err => {
+        console.log(err);
+    });
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,8 +1,95 @@
+const {
+    validationResult
+} = require('express-validator');
+
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) => {
-    res.status(200).json({
-        posts: [{
-            title: 'First Title',
-            contents: 'First Contents'
-        }]
-    });
+    Post
+        .find()
+        .then(posts => {
+            // if (!posts) {
+            //     res.status(200).json({
+            //         posts: [],
+            //         message: 'No Posts'
+            //     });
+            // }
+            Post.countDocuments()
+                .then(docs => {
+                    return res.status(200).json({
+                        posts: posts,
+                        totalPosts: docs
+                    });
+                })
+        })
+        .catch(err => console.log(err))
+
+    // res.status(200).json({
+    //     posts: [{
+    //         _id: '1',
+    //         title: 'First Title',
+    //         contents: 'First Contents',
+    //         imageUrl: 'images/POLICY.png',
+    //         creator: {
+    //             name: 'Mayuraksha Sikdar'
+    //         },
+    //         createdAt: new Date()
+    //     }]
+    // });
+}
+
+exports.createPost = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        const error = new Error('Validation Failed. Please enter correct data.');
+        error.statusCode = 422;
+        throw error;
+    }
+    const title = req.body.title;
+    const content = req.body.content;
+    const post = new Post({
+        title: title,
+        content: content,
+        imageUrl: 'images/POLICY.png',
+        creator: {
+            name: 'Mayuraksha Sikdar'
+        }
+    })
+    post.save()
+        .then(result => {
+            res.status(201).json({
+                message: 'Post Create Success',
+                post: result
+            });
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        });
+
+}
+
+
+exports.getPost = (req, res, next) => {
+    const postId = req.params.postId;
+    Post.findById(postId)
+        .then(post => {
+            if (!post) {
+                const error = new Error('Could not find post.');
+                error.statusCode = 404;
+                throw error;
+            }
+            res.status(200).json({
+                message: 'Post Fetch',
+                post: post
+            })
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        });
 }
